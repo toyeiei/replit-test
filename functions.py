beaa691cc2d2660945113d863f2c5a3dfd264f13
Kasidis Satangmongkol <eskimo.toy@gmail.com>
2025-12-20 00:06:10 +0000

    create new functions

---
+++ functions.py
@@ -0,0 +1,6 @@
+def greeting():
+    print("hello world")
+
+
+def add_two(num):
+    return num + 2
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# import function from another files
+from functions import add_two
+
 # this is my first commit
 print("hello world")
 
@@ -6,3 +9,6 @@ print("aloha!")
 
 # print goodbye
 print("good bye")
+
+# use add_two
+print(add_two(5), add_two(10))
